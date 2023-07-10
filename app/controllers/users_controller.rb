5721ab6bec1624e5f46a5b380dcf9478445632ce
hiepdd <dao.duc.hiep@sun-asterisk.com>
2023-07-10 15:05:57 +0700

    Finish chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :load_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
 
-    flash[:danger] = t "user_not_found"
-    redirect_to root_path
+  def index
+    @pagy, @users = pagy User.all
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -21,10 +24,60 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "edit_user.user_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.user_deleted"
+    else
+      flash[:danger] = t "users.user_delete_failed"
+    end
+    redirect_to users_path
+  end
+
   private
   def user_params
     # Strong Parameters
     params.require(:user)
           .permit :name, :email, :password, :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t "user_not_found"
+    redirect_to root_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "please_login"
+    redirect_to login_path
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:danger] = t "not_correct_user"
+    redirect_to root_path
+  end
+
+  def admin_user
+    return if current_user.admin?
+
+    flash[:danger] = t "action_not_allowed"
+    redirect_to root_path
   end
 end
