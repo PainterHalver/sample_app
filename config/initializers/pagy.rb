445672ea5baeeeec5e1a1ab84411c31de5c76e61
hiepdd <dao.duc.hiep@sun-asterisk.com>
2023-07-12 21:03:32 +0700

    Finish chapter 14

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,7 @@
 require "pagy/extras/bootstrap"
+require "pagy/extras/overflow"
 
+Pagy::DEFAULT[:overflow] = :last_page
 Pagy::I18n.load({locale: "en"},
                 {locale: "vi",
                  filepath: Rails.root.join("config/locales/pagy-vi.yml")})
